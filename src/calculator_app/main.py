012080966182831f7af1f57f41be7db12816b9cb
Matheus Henrique Fonseca Afonso <henrique.matheus@gec.inatel.br>
2024-10-20 15:31:53 -0300

    feat(project): Buttons display
    The display of buttons
    where redimensioned and
    the style of them
    are changed.

--- src/calculator_app/main.py
+++ src/calculator_app/main.py
@@ -3,49 +3,60 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Calc App"
-    result = ft.Text(value="0")
+
+    result = ft.Text(value="0", color=ft.colors.WHITE, size=20)
 
     page.add(
-        ft.Row(controls=[result]),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="AC"),
-                ft.ElevatedButton(text="+/-"),
-                ft.ElevatedButton(text="%"),
-                ft.ElevatedButton(text="/"),
-            ]
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="7"),
-                ft.ElevatedButton(text="8"),
-                ft.ElevatedButton(text="9"),
-                ft.ElevatedButton(text="*"),
-            ]
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="4"),
-                ft.ElevatedButton(text="5"),
-                ft.ElevatedButton(text="6"),
-                ft.ElevatedButton(text="-"),
-            ]
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(text="1"),
-                ft.ElevatedButton(text="2"),
-                ft.ElevatedButton(text="3"),
-                ft.ElevatedButton(text="+"),
-            ]
-        ),
-        ft.Row(
-             controls=[
-                ft.ElevatedButton(text="0"),
-                ft.ElevatedButton(text="."),
-                ft.ElevatedButton(text="="),
-            ]
-        ),
+        ft.Container(
+            width=350,
+            bgcolor=ft.colors.BLACK,
+            border_radius=ft.border_radius.all(20),
+            padding=20,
+            content=ft.Column(
+                controls=[
+                    ft.Row(controls=[result], alignment="end"),
+                    ft.Row(
+                        controls=[
+                            ft.ElevatedButton(text="AC", expand=1),
+                            ft.ElevatedButton(text="+/-", expand=1),
+                            ft.ElevatedButton(text="%", expand=1),
+                            ft.ElevatedButton(text="/", expand=1),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            ft.ElevatedButton(text="7", expand=1),
+                            ft.ElevatedButton(text="8", expand=1),
+                            ft.ElevatedButton(text="9", expand=1),
+                            ft.ElevatedButton(text="*", expand=1),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            ft.ElevatedButton(text="4", expand=1),
+                            ft.ElevatedButton(text="5", expand=1),
+                            ft.ElevatedButton(text="6", expand=1),
+                            ft.ElevatedButton(text="-", expand=1),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            ft.ElevatedButton(text="1", expand=1),
+                            ft.ElevatedButton(text="2", expand=1),
+                            ft.ElevatedButton(text="3", expand=1),
+                            ft.ElevatedButton(text="+", expand=1),
+                        ]
+                    ),
+                    ft.Row(
+                        controls=[
+                            ft.ElevatedButton(text="0", expand=2),
+                            ft.ElevatedButton(text=".", expand=1),
+                            ft.ElevatedButton(text="=", expand=1),
+                        ]
+                    ),
+                ] 
+            ),
+        )
     )
 
 
